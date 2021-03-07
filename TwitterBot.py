c190a700d6c9d7fb2806492c617d76e597e6519b
Yeong Zhen Hong <yeongzhenhong@gmail.com>
2021-03-07 21:05:06 +0800

    Edited TwitterBot.py to change it to OO format to allow it to be called by other classes

--- TwitterBot.py
+++ TwitterBot.py
@@ -13,14 +13,28 @@ import json
 import auth_token
 
 
-def authenticate():
-    auth = OAuthHandler(auth_token.CONSUMER_KEY, auth_token.CONSUMER_SECRET)
-    auth.set_access_token(auth_token.ACCESS_TOKEN, auth_token.ACCESS_TOKEN_SECRET)
-    api = API(auth)
-    return api
-
-
 class Analyze():
+    '''
+    Analyze takes in user input as keyword, and generates tweets.json file
+    '''
+    def __init__(self,keyword):
+        super().__init__()
+        self.keyword=keyword
+    '''
+    @authenticate() takes in twitterAPI keys to grant the application access
+    to twitter API
+    '''
+
+    def authenticate(self):
+        auth = OAuthHandler(auth_token.CONSUMER_KEY,
+                            auth_token.CONSUMER_SECRET)
+        auth.set_access_token(auth_token.ACCESS_TOKEN,
+                              auth_token.ACCESS_TOKEN_SECRET)
+        api = API(auth)
+        return api
+    '''
+    @tweets_to_df(tweets) takes in a python list and generates a json file  
+    '''
     def tweets_to_df(self, tweets):
         df = pd.DataFrame(data=[tweet.text for tweet in tweets])
         df['who'] = np.array([tweet.user.screen_name for tweet in tweets])
@@ -28,20 +42,20 @@ class Analyze():
         df['retweets'] = np.array([tweet.retweet_count for tweet in tweets])
         df['likes'] = np.array([tweet.favorite_count for tweet in tweets])
         return df.to_json('tweets.json', orient='records', indent=1)
-
-
-if __name__ == "__main__":
-    tweet_analyzer = Analyze()
-    keyword = "Austin OR Trump"
-    filter_list = keyword + " -filter:retweets"
-    api = authenticate()
-    tweets = Cursor(api.search, q=filter_list, lang="en", result_type="popular").items(10)
-    tweetLi = []
-    for tweet in tweets:
-        tweetLi.append(tweet)
-    df = tweet_analyzer.tweets_to_df(tweetLi)
-    print(dir(tweetLi[0]))
-
-
-
+    '''
+    @initTwitter function takes calls authenticate() to allow the application
+    to access twitter api 
+    and calls tweets_to_df() function to generate a json file
+    '''
+    def initTwitter(self):
+        filter_list = self.keyword + " -filter:retweets"
+        api = self.authenticate()
+        tweets = Cursor(api.search, q=filter_list, lang="en",
+                        result_type="popular").items(10)
+        tweetLi = []
+        for tweet in tweets:
+            tweetLi.append(tweet)
+        df = self.tweets_to_df(tweetLi)
+        # print(dir(tweetLi[0]))
+        #print statement for output purposes, will delete after
 
