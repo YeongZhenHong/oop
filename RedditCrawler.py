b42467f79b914cd2a82ccd2e9b69ef92b665c111
Kennn7777777 <kennnnnang@gmail.com>
2021-03-21 03:45:47 +0800

    use set_Setting Func from superclass and added some changes to outputToFile func

--- RedditCrawler.py
+++ RedditCrawler.py
@@ -1,6 +1,6 @@
 """! 
 @file RedditCrawler.py
-@author Kendrick Ang 2609737A
+@author Kendrick Ang 2609737A / Sim Wei Jun Austin  2609730S
 @brief This file contains the Reddit Crawler sub class
 @version 1.0
 @section DESCRIPTION
@@ -11,7 +11,9 @@ e.g redditC = RedditCrawler()
     redditC.crawl()
 """
 
+from logging import raiseExceptions
 import praw
+from prawcore import PrawcoreException
 import csv
 from Crawler import Crawler
 from datetime import datetime
@@ -32,10 +34,12 @@ class RedditCrawler(Crawler):
         super().__init__()
         #store a list of objects
         self.posts = []
-        self.limit = limit
+        self.subreddit1 = None
+        self.submission = None
+
         #authenticates and creates an instance of reddit so that we can start to scrape data 
         self.authenticate(Auth_Token.REDDIT_CLIENT,Auth_Token.REDDIT_SECRET, Auth_Token.REDDIT_AGENT)
-    
+
     def authenticate(self, clientid, clientsecret, user):
         """! authenticates the usage of scraping data from reddit and creates an instance of reddit
         @param clientid     input client_id from reddit
@@ -44,38 +48,33 @@ class RedditCrawler(Crawler):
         """
         #creates an instance of reddit with the specific information 
         self.reddit = praw.Reddit(client_id=clientid, client_secret=clientsecret, user_agent=user)
-
+ 
     def setSettings(self, searchString, limit):
         """! sets the search string and limit
         @param searchString  the search string data we want to crawl 
         @param limit         the amount of posts that can be crawled
         """
-        #sets the search string in the super class
-        super().set_searchString(searchString)
-        #limits the amount of post to be crawled
-        self.setLimit(limit)
-
-    def setLimit(self, limit):
-        """! sets the limit to amount of posts that can be crawled
-        @param limit the amount of posts that can be crawled
-        """
-        self.limit = limit
-
+        #sets the searchString and searchLimit values in the super class
+        super().set_Settings(searchString, limit)
+        
     def crawl(self):
         """! main function to start crawling data and export it to .csv file
         """
+        if super().get_searchLimit() <= 0: 
+            raise ValueError("Error, that is not a positive number!") 
         #navigates to subreddit "singapore", sets the search string and limits the posts
-        subreddit = self.reddit.subreddit("singapore")
-        submission = subreddit.search(super().get_searchString(), limit=self.limit)
-
-        #in every submission, scrape the data we needed (comments, time, date)
-        for post in submission:
-            title = post.title
-            url = post.permalink
-            commentCount = post.num_comments
-            #upvotes = post.score
-            
-            try:
+        try:
+            self.subreddit1 = self.reddit.subreddit("singapore")
+            self.submission = self.subreddit1.search(super().get_searchString(), limit=super().get_searchLimit())
+           
+            #in every submission, scrape the data we needed (comments, time, date)
+            for post in self.submission:
+                title = post.title
+                url = post.permalink
+                commentCount = post.num_comments
+                #upvotes = post.score
+                
+                post.comments.replace_more(limit=None)
                 for comment in post.comments.list():
                     date = str(datetime.fromtimestamp(comment.created_utc))
                     datesplit = date.split(" ")
@@ -83,8 +82,8 @@ class RedditCrawler(Crawler):
                     p = (comment.body, comment.score, datesplit[0], datesplit[1])
                     # p = (title, comment.body, url, commentCount, datesplit[0], datesplit[1])
                     self.posts.append(p)
-            except: 
-                pass
+        except Exception as err:
+           print(err)
         
         #output data to a .csv file
         self.outputToFile()
@@ -100,5 +99,5 @@ class RedditCrawler(Crawler):
                 #writer.writerow(['Comment', 'Datetime',])
                 #writer.writerow(['Title', 'Comment', 'Link', 'Comment Count', 'Date', 'Time'])
                 writer.writerows(self.posts)
-        except:
-            print("Something went wrong")
+        except Exception as e:
+            print(e)
