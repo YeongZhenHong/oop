bfea091c46da6d69050ca749a67ea25bffc77940
Kennn7777777 <kennnnnang@gmail.com>
2021-03-20 19:26:34 +0800

    added upvotes count per comment

--- RedditCrawler.py
+++ RedditCrawler.py
@@ -23,17 +23,17 @@ class RedditCrawler(Crawler):
     
     Inherits from crawler based class test
     """
-
     def __init__(self, limit=10):
         """! The reddit crawler class initializer
         @param limit The amount of posts that can be crawled (optional)
         @return An instance of reddit crawler class initialized with specified limit and authenticates
         """
+        #initialize super class
         super().__init__()
         #store a list of objects
         self.posts = []
         self.limit = limit
-    
+        #authenticates and creates an instance of reddit so that we can start to scrape data 
         self.authenticate(Auth_Token.REDDIT_CLIENT,Auth_Token.REDDIT_SECRET, Auth_Token.REDDIT_AGENT)
     
     def authenticate(self, clientid, clientsecret, user):
@@ -42,6 +42,7 @@ class RedditCrawler(Crawler):
         @param clientsecret input secret key from reddit
         @param user         input user_agent(name) from reddit
         """
+        #creates an instance of reddit with the specific information 
         self.reddit = praw.Reddit(client_id=clientid, client_secret=clientsecret, user_agent=user)
 
     def setSettings(self, searchString, limit):
@@ -49,7 +50,9 @@ class RedditCrawler(Crawler):
         @param searchString  the search string data we want to crawl 
         @param limit         the amount of posts that can be crawled
         """
+        #sets the search string in the super class
         super().set_searchString(searchString)
+        #limits the amount of post to be crawled
         self.setLimit(limit)
 
     def setLimit(self, limit):
@@ -61,22 +64,29 @@ class RedditCrawler(Crawler):
     def crawl(self):
         """! main function to start crawling data and export it to .csv file
         """
+        #navigates to subreddit "singapore", sets the search string and limits the posts
         subreddit = self.reddit.subreddit("singapore")
         submission = subreddit.search(super().get_searchString(), limit=self.limit)
 
+        #in every submission, scrape the data we needed (comments, time, date)
         for post in submission:
             title = post.title
             url = post.permalink
             commentCount = post.num_comments
+            #upvotes = post.score
+            
             try:
                 for comment in post.comments.list():
                     date = str(datetime.fromtimestamp(comment.created_utc))
-                    # datesplit = date.split(" ")
-                    p = (comment.body, date)
+                    datesplit = date.split(" ")
+                    #p = (comment.body, date)
+                    p = (comment.body, comment.score, datesplit[0], datesplit[1])
                     # p = (title, comment.body, url, commentCount, datesplit[0], datesplit[1])
                     self.posts.append(p)
-            except: pass
+            except: 
+                pass
         
+        #output data to a .csv file
         self.outputToFile()
 
     def outputToFile(self,filename="reddit"):
@@ -86,8 +96,9 @@ class RedditCrawler(Crawler):
         try:
             with open(super().get_searchString() + '_' + filename + '.csv', 'w', newline='', encoding='utf-8') as f:
                 writer = csv.writer(f)
-                writer.writerow(['Comment', 'Datetime'])
-                # writer.writerow(['Title', 'Comment', 'Link', 'Comment Count', 'Date', 'Time'])
+                writer.writerow(['Comment', 'Upvotes', 'Date', 'Time'])
+                #writer.writerow(['Comment', 'Datetime',])
+                #writer.writerow(['Title', 'Comment', 'Link', 'Comment Count', 'Date', 'Time'])
                 writer.writerows(self.posts)
         except:
-            raise FileNotFoundError('File cannot be opened!')
+            print("Something went wrong")
