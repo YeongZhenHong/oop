9dd45dd3281e64cc7a42fac1b5bd3236c2748fd4
orhstin <57675090+orhstin@users.noreply.github.com>
2021-03-21 16:03:17 +0800

    Halfway done with test_twitter cases

--- TestTwitter.py
+++ TestTwitter.py
@@ -1,23 +1,51 @@
+"""!
+@file testCaseTwitter.py
+@author Sim Wei Jun Austin 2609730S
+@brief This file contains the Twitter test case class.
+@version 1.0
+@section DESCRIPTION
+Running of Twitter Crawler test cases
+"""
+
+
 import unittest
-from unittest.mock import Mock
+from unittest.mock import Mock, patch
 from TwitterCrawler import Twitter
 
 class TestTwitter(unittest.TestCase):
+        """! The Twitter test case class
+        @brief Defining a TestTwitter class to test Twitter Crawler functions.
+        """
         def setUp(self):
             self.twitter = Twitter()
-            pass
+            print("Setting up Twitter Crawler...")
         
-        def test_setSettings(self):
-            self.twitter.setSettings("testCase", 10)
-            self.assertEqual(self.twitter.get_searchString(),"testCase")
-            self.assertEqual(self.twitter.limit, 10)
-            self.assertNotEqual(self.twitter.get_searchString(),"nottestCase")
-            self.assertNotEqual(self.twitter.limit, 1)
-
+        def tearDown(self):
+            print("Tearing down Twitter Crawler...")
+        
         def test_authenticate(self):
-            pass
+            self.twitter.authenticate()
+             
+        def test_set_Settings(self):
+            print("Testing set_Settings")
+            self.twitter.set_Settings("testCase", 10)
+            print("set_Settings called!")
+            self.assertEqual(self.twitter.get_searchString(),"testCase")
+            self.assertNotEqual(self.twitter.get_searchString(),"nottestCase")
+            print("get_searchString valid!")
+            self.assertEqual(self.twitter.get_searchLimit(), 10)
+            self.assertNotEqual(self.twitter.get_searchLimit(), 1)
+            print("get_searchLimit valid!")
+        
+        @patch.object(Twitter, 'tweets_to_df', return_value=None)
+        def test_tweets_to_df(self, mock_method):
+            testLi = ['test','case']
+            self.twitter.tweets_to_df(testLi)
+            mock_method.assert_called_with(testLi)
+            print("test_tweets_to_df valid!")
 
-        def test_crawl(self):
+        @patch.object(Twitter, 'crawl', return_value=None)
+        def test_crawl(self, mock_method):
             pass
 
 if __name__ == "__main__":
